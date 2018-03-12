cedbd7acf4131c5c01d42483310468ad8705a9af
java <java@Ipartek-TOSH02.ipartekaula.com>
2018-03-12 13:28:58 +0100

    primerBoot Paises

---
+++ primerBoot/src/main/java/com/ipartek/primerBoot/App.java
@@ -0,0 +1,11 @@
+package com.ipartek.primerBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class App {public static void main( String[] args ){SpringApplication.run(App.class, args);}}
---
+++ primerBoot/src/main/java/com/ipartek/primerBoot/service/PaisService.java
@@ -0,0 +1,14 @@
+package com.ipartek.primerBoot.service;
+import java.util.List;
+
+
+
+import com.ipartek.primerBoot.domain.Pais;
+
+
+public interface PaisService {
+
+	public List<Pais> getPaises();
+	public Pais getPaisByNombre(String nombre);
+	
+}
